b6826d449f752c7eaf1d12a16c411ef4bd94709f
LuisDiazR-Dev <diazr.info@gmail.com>
2024-08-21 21:13:45 -0500

    ajustes iniciales de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,19 @@
-import { useSelector, useDispatch } from 'react-redux'
-import {
-	increment,
-	decrement,
-	reSet,
-} from './Redux/features/counter/Reducer-CounterSlice'
-import {
-	incrementAsync,
-	incrementPar,
-} from './Redux/features/counter/Actions-Counter'
+import { Routes, Route } from 'react-router-dom'
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Landing from './pages/Landing'
 
 function App() {
-	const count = useSelector((state) => state.counter.value)
-	const dispatch = useDispatch()
-
+	// if (isLoading) return <div>Loading...</div>
 	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<span>count is {count}</span>
-				<div>
-					<hr />
-					<button onClick={() => dispatch(increment())}>Mas +</button>
-					<button onClick={() => dispatch(decrement())}>Menos -</button>
-					<button onClick={() => dispatch(reSet())}>Reset</button>
-					<button onClick={() => dispatch(incrementAsync(5))}>
-						Incrementar en 5 después de 1 segundo
-					</button>
-					<button onClick={() => dispatch(incrementPar(count))}>
-						aumentar si es par
-					</button>
-				</div>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</>
+		<div className="container mx-auto">
+			<Navbar />
+			<hr />
+			<Routes>
+				<Route path="/" element={<Landing />} />
+			</Routes>
+		</div>
 	)
 }
 
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,9 @@
+const Landing = () => {
+	return (
+		<div>
+			<h2>landing page</h2>
+		</div>
+	)
+}
+
+export default Landing
