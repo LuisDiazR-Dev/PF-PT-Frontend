98131c3110ae9073885bbf2e14d0ba38c154fea0
JHOSEPH <jhoseph@localhost.localdomain>
2024-08-25 02:13:10 -0400

    fix: admin y cree directorios para admin

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
+import { Condominium } from "../admin/Condominium.jsx";
+import { Notification } from "../admin/Notification.jsx";
+import { Payment } from "../admin/Payment.jsx";
+import { Service } from "../admin/Service.jsx";
+import { Statistics } from "../admin/Statistics.jsx";
+import { Apart } from "../admin/Apart.jsx";
 
 
 const Routes = () => {
@@ -7,6 +13,32 @@ const Routes = () => {
     {
       path: "/",
       element: <App />,
+    },
+    {
+      path:"/apartamento",
+      element:<Apart/>,
+    },
+    
+    {
+      path: "/notificaciones",
+      element: <Notification/>,
+    },
+    {
+      path: "/servicio",
+      element: <Service />,
+      
+    },
+    {
+      path: "/estadistica",
+      element: <Statistics />,
+    },
+    {
+      path: "/pagos",
+      element: <Payment/>,
+    },
+    {
+      path: "/addcondominio",
+      element: <Condominium/>,
     },
    
   ];
